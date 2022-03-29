c9ea8a32eca27d6722f3e6bbd0708e34260931ae
niumin0211 <857210963@qq.com>
2022-03-29 23:16:48 +0800

    form框架-test

--- src/pages/my-form/MyFormPage.js
+++ src/pages/my-form/MyFormPage.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-29 11:51:08
- * @LastEditTime: 2022-03-29 15:05:22
+ * @LastEditTime: 2022-03-29 23:14:03
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /my-app-learn/src/pages/my-form/MyFormPage.js
@@ -32,7 +32,7 @@ function MyFormPage() {
     return (
         <div>
             <h3>
-                nium form
+                nium form page
             </h3>
             <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
                 <Field name="username">
